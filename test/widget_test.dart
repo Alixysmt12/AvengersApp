03cbacc734983423b87addbe2e3899e0f3052278
Muhammad ali <alimuhammad5022@gmail.com>
2025-07-05 01:27:12 +0500

    Trigger CI/CD

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,6 +11,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:avengers_app/main.dart';
 
 void main() {
+  test('Sample test', () {
+    expect(2 + 2, 4);
+  });
   /*testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
